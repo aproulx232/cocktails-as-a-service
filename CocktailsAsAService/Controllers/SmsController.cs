26cfaddc0d42f34145e800923b4cd772c9e7cedc
Allen Proulx <Allen.Proulx@adesa.com>
2022-06-16 08:56:38 -0400

    Add mock responses

--- CocktailsAsAService/Controllers/SmsController.cs
+++ CocktailsAsAService/Controllers/SmsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Twilio.AspNet.Common;
 using Twilio.AspNet.Core;
 using Twilio.TwiML;
 
@@ -9,10 +10,37 @@ namespace CocktailsAsAService.Controllers
     public class SmsController : TwilioController
     {
         [HttpPost]
-        public TwiMLResult Index()
+        public TwiMLResult Index(SmsRequest request)
+        {
+            var message = request.Body.ToLowerInvariant();
+
+            return message switch
+            {
+                "help" => GetHelpResponse(),
+                "save" => AddToFavoritesResponse(),
+                _ => GetCocktailResponse()
+            };
+        }
+
+        private TwiMLResult GetHelpResponse()
         {
             var messagingResponse = new MessagingResponse();
-            messagingResponse.Message("The Robots are coming! Head for the hills!");
+            messagingResponse.Message("Please list cocktail name");
+
+            return TwiML(messagingResponse);
+        }
+
+        private TwiMLResult AddToFavoritesResponse()
+        {
+            var messagingResponse = new MessagingResponse();
+            messagingResponse.Message("Cocktail will be added to favorites list");
+
+            return TwiML(messagingResponse);
+        }
+        private TwiMLResult GetCocktailResponse()
+        {
+            var messagingResponse = new MessagingResponse();
+            messagingResponse.Message("5 mint leaves, more for garnish\r\n2 ounces white rum\r\n1 ounce fresh lime juice\r\n½ ounce simple syrup\r\nIce\r\nClub soda or sparkling water\r\nLime slices, for garnish");
 
             return TwiML(messagingResponse);
         }
