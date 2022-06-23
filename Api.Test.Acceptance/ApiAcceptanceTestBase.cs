fcc4ecce43200d987bb966519a8035b0ebf7b01a
Allen Proulx <Allen.Proulx@adesa.com>
2022-06-23 09:06:35 -0400

    Added Api Acceptance Tests

---
+++ Api.Test.Acceptance/ApiAcceptanceTestBase.cs
@@ -0,0 +1,73 @@
+﻿using FluentAssertions;
+using Infrastructure.CocktailDbService;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Test.Acceptance
+{
+    public class ApiAcceptanceTestBase : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        protected readonly HttpClient HttpClient;
+        protected HttpResponseMessage? HttpResponse;
+        protected const string CocktailDbBaseUrl = "https://thecocktaildb-mock.com";
+
+        protected readonly Mock<HttpMessageHandler> MockCocktailDbHttpMessageHandler = new();
+
+        protected ApiAcceptanceTestBase(WebApplicationFactory<Startup> factory)
+        {
+            HttpClient = factory
+                         .WithWebHostBuilder(webHostBuilder =>
+                         {
+                             webHostBuilder
+                                 .ConfigureTestServices(serviceCollection =>
+                                 {
+                                     serviceCollection.AddSingleton(BuildMockHttpClientFactory());
+                                 });
+                         })
+                         .CreateClient();
+        }
+
+        private IHttpClientFactory BuildMockHttpClientFactory()
+        {
+            var mockHttpClientFactory = new Mock<IHttpClientFactory>();
+
+            var mockCocktailDbToggleHttpClient =
+                new HttpClient(MockCocktailDbHttpMessageHandler.Object)
+                {
+                    BaseAddress = new Uri(CocktailDbBaseUrl)
+                };
+            mockHttpClientFactory
+                .Setup(httpClientFactory => httpClientFactory.CreateClient(nameof(ICocktailDbService)))
+                .Returns(mockCocktailDbToggleHttpClient);
+
+            return mockHttpClientFactory.Object;
+        }
+
+        protected static StringContent? BuildRequestContent(object request)
+        {
+            return new StringContent(JsonConvert.SerializeObject(request),
+                                     Encoding.UTF8,
+                                     "application/json");
+        }
+
+        protected async Task ThenResponseIs(HttpStatusCode expectedStatusCode, string? expectedContent = null)
+        {
+            HttpResponse?.StatusCode.Should().Be(expectedStatusCode);
+         
+            if (expectedContent != null)
+            {
+                var responseContent = await HttpResponse!.Content.ReadAsStringAsync();
+                responseContent.Should().Be(expectedContent);
+            }
+        }
+    }
+}
---
+++ Api.Test.Acceptance/MockHelper.cs
@@ -0,0 +1,46 @@
+﻿using Moq;
+using Moq.Protected;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Api.Test.Acceptance
+{
+    public static class MockHelper
+    {
+        public static void SetupHttpCall(Mock<HttpMessageHandler> mockHttpMessageHandler, string requestUrl, HttpResponseMessage httpResponseMessage)
+        {
+            mockHttpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.Is<HttpRequestMessage>(requestMessage => requestMessage.RequestUri == new Uri(requestUrl)),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(httpResponseMessage);
+        }
+
+        public static void SetupHttpCallWithCallback(Mock<HttpMessageHandler> mockHttpMessageHandler, string requestUri, HttpResponseMessage httpResponseMessage, Action<HttpRequestMessage, CancellationToken> callback)
+        {
+            mockHttpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.Is<HttpRequestMessage>(requestMessage => requestMessage.RequestUri == new Uri(requestUri)),
+                    ItExpr.IsAny<CancellationToken>())
+                .Callback(callback)
+                .ReturnsAsync(httpResponseMessage);
+        }
+
+        public static void VerifyHttpCall(Mock<HttpMessageHandler> mockHttpMessageHandler, HttpMethod requestHttpMethod, string requestUrl, Times timesCalled)
+        {
+            mockHttpMessageHandler
+                .Protected()
+                .Verify("SendAsync",
+                        timesCalled,
+                        ItExpr.Is<HttpRequestMessage>(request => request.Method == requestHttpMethod &&
+                                                                 request.RequestUri == new Uri(requestUrl)),
+                        ItExpr.IsAny<CancellationToken>());
+        }
+    }
+}
